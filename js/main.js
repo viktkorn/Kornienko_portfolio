3413301399d053f2b6ff38e42a156729f1f75d6f
Viktoria Kornienko <89094100+viktkorn@users.noreply.github.com>
2021-11-06 11:36:37 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,27 +2,41 @@ $(function () {
   $('.slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    adaptiveHeight: true,
+    draggable: false,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 2000, 
     prevArrow: '<button class="slider__arrow slider__arrow-prev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="slider__arrow slider__arrow-next"><img src="images/arrow-right.svg" alt=""></button>',
     responsive:[
       {
-        breakpoint: 1600,
+        breakpoint: 1601,
         settings:{
-          slidesToShow: 3,
+          slidesToShow: 3
         }
       },
       {
-        breakpoint: 870,
+        breakpoint: 1201,
+        settings:{
+          slidesToShow: 3,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 920,
         settings:{
         slidesToShow: 2,
         dots: true
         }      
       },
       {
-        breakpoint: 590,
+        breakpoint: 620,
         settings:{
         slidesToShow: 1,
-        dots: true  
+        dots: true,
+        centerMode: true,
+        variableWidth: true  
         }      
       }
     ]
